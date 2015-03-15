f1f7c7716608028f9d0f451bf2c0e5e0c0789f2c
bastianbertram <bastianbertram@bastians-mbp.wireless.unimaas.local>
2015-03-15 14:59:18 +0100

    Generated first execution class to test the bentley algorithm

---
+++ src/Controller/BentleyExecutor.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Controller;
+
+/**
+ * @author bastianbertram
+ *
+ */
+public class BentleyExecutor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
