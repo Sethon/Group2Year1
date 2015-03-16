55d407cb21039b51fb295166ea38ac230a6f2ffa
Ricardsb Marcinkevics <RMarcinkevics@ricardss-mbp.wireless.unimaas.local>
2015-03-16 12:23:06 +0100

    Segment length calculation method

--- src/Model/Helper.java
+++ src/Model/Helper.java
@@ -1,5 +1,8 @@
 package Model;
 
 public class Helper {
-
+	public static double length(Point2D p0, Point2D p1) {
+		return Math.sqrt(Math.pow(p1.getX() - p0.getX(),2) + 
+		Math.pow(p1.getY() - p0.getY(),2));
+	}
 }
